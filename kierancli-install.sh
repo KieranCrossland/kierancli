a3214d665c6f0ce4a62eb141889f7c4463519dff
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2022-07-11 01:55:32 +0100

    changed kierancli to subfolder kieran_crossland

--- kierancli-install.sh
+++ kierancli-install.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 #Information section
+echo "Welcome to the installer for kierancli."
+sleep 2
+echo "SOFTWARE INFORMATION"
+sleep 0.2
 echo ""
-echo SOFTWARE DETAILS
-echo ""
-sleep 0.7
-echo "name = 'kierancli'"
-sleep 0.7
-echo "authors = ['Kieran Crossland <kierancrossland1st@gmail.com>']" 
-sleep 0.7
-echo "license = 'AGPL-3.0'"
-sleep 0.7
-echo "repository = 'https://github.com/kierancrossland/kierancli/'"
-sleep 0.7
+echo "Contact email 'kierancrossland1st@gmail.com'" 
+sleep 0.2
+echo "License = 'GNU AGPL-3.0'"
+sleep 0.2
+echo "source repository = 'https://github.com/kierancrossland/kierancli/'"
 echo ""
 echo ""
 
@@ -19,22 +17,22 @@ echo ""
 echo BEGINNING INSTALL / UPGRADE
 echo ""
 sleep 0.5
-echo "running rm -rf ~/.local/share/kierancli/"
+echo "Deleting prior versions in ~/.local/share/"
 rm -rf ~/.local/share/kierancli/
-git clone https://github.com/kierancrossland/kierancli ~/.local/share/kierancli/
-cd ~/.local/share/kierancli
+rm -rf ~/.local/share/kieran_crossland/kierancli/
+git clone https://github.com/kierancrossland/kierancli ~/.local/share/kieran_crossland/kierancli/
+cd ~/.local/share/kieran_crossland/kierancli
 echo "If install fails here then Cargo is not presesnt."
 echo "running cargo build --release"
 cargo build --release
-mkdir ~/.local/share/kierancli/bin
-mv ~/.local/share/kierancli/target/release/kierancli ~/.local/share/kierancli/bin/
 echo ""
 echo attempting to symlink binary to /usr/local/bin/
-echo symlink could fail on BSD/Unix due to lack of sudo
-sudo ln -sf ~/.local/share/kierancli/bin/kierancli /usr/local/bin/kierancli
+echo symlink could fail on other Unix systems due to their lack of sudo
+sudo ln -sf ~/.local/share/kieran_crossland/kierancli/target/release/kierancli /usr/local/bin/kierancli
 echo ""
-echo INSTALL SUCCEEDED
+pwd
 date > build_date
-echo "last build date @ ~/.local/share/kierancli/build_date"
+echo "last build/install date @ ~/.local/share/kieran_crossland/kierancli/build_date"
 echo ""
-echo run kierancli
+which kierancli
+echo Program installed, run kierancli.
