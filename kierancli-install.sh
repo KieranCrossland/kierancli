0416756f76973e22e796a67f4b9d6099ac09a4cc
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2022-07-11 02:22:13 +0100

    Update kierancli-install.sh

--- kierancli-install.sh
+++ kierancli-install.sh
@@ -23,5 +23,5 @@ sudo ln -sf ~/.local/share/kieran_crossland/kierancli/target/release/kierancli /
 date > build_date
 echo "last build/install date @ ~/.local/share/kieran_crossland/kierancli/build_date"
 which kierancli
-echo "Ignore all compiler warnings, they are either your fault (fix them) or my fault for using depreciated functions"
+echo "Ignore all compiler warnings, they are either your fault (fix them) or my fault for using depreciated functions. I love my depreciated functions and probably will keep using them."
 echo Program installed, run kierancli.
