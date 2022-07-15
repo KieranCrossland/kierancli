31a73e975f52e8b9c4fc9ae82c68c12f632c06c7
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2022-07-15 17:00:36 +0100

    Update kierancli-install.sh

--- kierancli-install.sh
+++ kierancli-install.sh
@@ -5,11 +5,12 @@ echo "Welcome to the Unix installer for kierancli."
 sleep 3
 echo "Information can be found @ ~/.local/share/kieran_crossland/kierancli/Cargo.toml"
 #Install section
-echo BEGINNING INSTALL
+echo "."
+echo BEGINNING BUILD & INSTALL
 cd ~/.local/share/
 pwd
 sleep 1
-echo "Deleting prior versions in ~/.local/share/"
+echo "Configuring directories in ~/.local/share/"
 rm -rf ~/.local/share/kierancli/
 rm -rf ~/.local/share/kieran_crossland/kierancli/
 git clone https://github.com/kierancrossland/kierancli ~/.local/share/kieran_crossland/kierancli/
@@ -22,6 +23,8 @@ echo symlink could fail on other Unix systems due to their lack of sudo
 sudo ln -sf ~/.local/share/kieran_crossland/kierancli/target/release/kierancli /usr/local/bin/kierancli
 date > build_date
 echo "Last build/install date @ ~/.local/share/kieran_crossland/kierancli/build_date"
-which kierancli
 echo "Ignore all compiler warnings, they are either your fault (fix them) or my fault for using depreciated functions. I love my depreciated functions and probably will keep using them."
+
+echo ""
+which kierancli
 echo Program installed, run kierancli.
