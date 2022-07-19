0b1b7cba91cfed45bfcd0426d9e741b664a57d8e
Kieran <85656267+KieranCrossland@users.noreply.github.com>
2022-07-19 11:46:30 +0100

    Update kierancli-install.sh

--- kierancli-install.sh
+++ kierancli-install.sh
@@ -3,7 +3,8 @@
 clear
 echo "Welcome to the Unix installer for kierancli."
 sleep 2
-echo "Information can be found @ ~/.local/share/kieran_crossland/kierancli/Cargo.toml"
+echo ""
+echo "Information @ ~/.local/share/kieran_crossland/kierancli/Cargo.toml"
 echo ""
 echo "Git repository @ https://github.com/KieranCrossland/kierancli/"
 #Install section
@@ -14,9 +15,9 @@ echo "Configuring directories in:"
 pwd
 echo ""
 rm -rf ~/.local/share/kierancli/
-rm -rf ~/.local/share/kieran_crossland/kierancli/
-git clone https://github.com/kierancrossland/kierancli ~/.local/share/kieran_crossland/kierancli/
-cd ~/.local/share/kieran_crossland/kierancli
+rm -rf ~/.local/share/kierancrossland/kierancli/
+git clone https://github.com/kierancrossland/kierancli ~/.local/share/kierancrossland/kierancli/
+cd ~/.local/share/kierancrossland/kierancli
 echo ""
 echo "If install fails here then Cargo is not presesnt."
 echo ""
@@ -25,10 +26,10 @@ echo ""
 cargo build --release
 echo attempting to symlink binary to /usr/local/bin/
 echo symlink could fail on other Unix systems due to their lack of sudo
-sudo ln -sf ~/.local/share/kieran_crossland/kierancli/target/release/kierancli /usr/local/bin/kierancli
+sudo ln -sf ~/.local/share/kierancrossland/kierancli/target/release/kierancli /usr/local/bin/kierancli
 date > build_date
 echo ""
-echo "Last build/install date @ ~/.local/share/kieran_crossland/kierancli/build_date"
+echo "Last build/install date @ ~/.local/share/kierancrossland/kierancli/build_date"
 echo ""
 echo "Ignore all compiler warnings, they are either your fault (fix them) or my fault for using depreciated functions. I love my depreciated functions and probably will keep using them."
 
